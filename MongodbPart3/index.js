6cc66d2c7d63c8e44fec3841923af4e1f6280217
firstRepo <hassanjawedwani@gmail.com>
2024-07-06 22:57:57 +0100

    CRUD_R_complete

--- MongodbPart3/index.js
+++ MongodbPart3/index.js
@@ -4,7 +4,12 @@ const app = express();
 const port = 8080;
 const Chat = require('./Models/chat');
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
 const mongoose = require('mongoose')
+
 main().then(()=>{
   console.log("Connection of Server with database at port 27017 is established")
 }).catch(err => console.log(err));
@@ -17,15 +22,12 @@ app.listen(port, () => {
   console.log("Server is listening at port 8080")
 });
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.send("root directory");
 })
 
 app.get("/chats", async (req, res) => {
-  const chat = await Chat.find();
-  console.log(chat)
-  res.send("chat directory");
+  const chats = await Chat.find();
+  res.render("chats", {chats});
 })
